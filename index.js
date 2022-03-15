db9d804d20957d5a67b077ec538ff85636891a27
Lorenzo Ciani <cianilorenzo3@gmail.com>
2022-03-15 16:26:01 +0100

    Chat implementata ma facaga a dio perchè sorcio è nocojone

--- index.js
+++ index.js
@@ -5,69 +5,111 @@ const bodyParser = require('body-parser')
 
 const schema = mongoose.Schema
 
+//? Declaration of message schema
+const messageSchema = new schema({
+    sender: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: false,
+        default: ""
+    },
+    receiver:
+    {
+        type: String
+    }
+})
+
 //? Declaration of user schema
 const userSchema = new schema({
     nickname: {
-        type: String
+        type: String,
+        required: true
+    },
+    messages: {
+        type: Map,
+        of: [messageSchema]
     }
 })
 
-const messageSchema = new schema({
-    nickname: {
-        type: String
-    },
-    message: {
-        type: String
-    },
-    date: {
-        type: Date
-    }
-})
-
-//? Mongoose conection initialization 
+//? Mongoose connection initialization 
 mongoose
-    .connect("mongodb://mongo:27017/new", { useNewUrlParser: true })
+    .connect("mongodb://localhost:27017/new", { useNewUrlParser: true })
     .then(() => console.log("MongoDB Connect"))
     .catch(err => console.log(err))
 
 const userItem = mongoose.model('user', userSchema)
 const messageItem = mongoose.model('message', messageSchema)
 
-const record1 = userItem({
-    nickname: "test"
-})
-record1.save().then(info => console.log(`record ${record1.nickname}`))
-
-//? Map to store user in key value pair
-const users = new Map();
-
 //? Consts Server configuration
 const PORT = 8080;
 const HOST = '0.0.0.0'
+
+// const userRecord = userItem({
+//     nickname: "Lorenzo",
+//     messages: []
+// })
+// userRecord.save().then(info => console.log(`Saved`))
 
 //?  App initialization
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-
 //! Mongo get TestRoute
+//TODO Fix this route to send correctly a date and not a string, or find a way to set fields not to null
 app.get('/chat', (req, res) => {
     messageItem.find().then(rec => res.send(rec)).catch(err => res.status(500).send('Internal Server error'))
-    //userItem.find().then(rec => res.send(rec)).catch(err => res.status(500).send("internal server error"))
 })
 
 //! Mongo post TestRoute
-app.post('/chat', (req, res) => {
+app.post('/chat/all', (req, res) => {
     console.log("Receiving message...")
+    let date = req.body.date ? req.body.date : Date.now()
     const messageRecord = messageItem({
-        nickname: req.body.nickname,
+        sender: req.body.nickname,
         message: req.body.message,
-        date: req.body.date
+        date: date
     })
-    messageRecord.save().then(info => console.log(`saved ${messageRecord.nickname}, ${messageRecord.message}`))
+    messageRecord.save().then(info => console.log(`saved ${info}`))
     res.send("Message received")
 })
+
+//! Mongo post to send a message
+app.post('/chat', (req, res) => {
+    console.log("Sending the message...")
+    let date = req.body.date ? req.body.date : Date.now()
+    const messageRecord = messageItem({
+        sender: req.body.nickname,
+        message: req.body.message,
+        date: date,
+        receiver: req.body.receiver
+    })
+    userItem.findOne({ nickname: req.body.receiver }, (err, user) => {
+        let messageToInsert = []
+        if (user.messages.get(req.body.nickname) != undefined) {
+            messageToInsert = user.messages.get(req.body.nickname)
+        }
+        messageToInsert.push(messageRecord)
+        user.messages.set(req.body.nickname, messageRecord)
+        user.save()
+    })
+    res.send("Private message sended")
+})
+
+//!Get for the private chat
+app.post('/chat/private', (req, res) => {
+
+
+    userItem.findOne({ nickname: req.body.nickname }).then(rec => res.send(rec)).catch(err => res.status(500).send('Internal Server error'))
+}
+)
 
 //! Get for dipendenti
 app.get('/', (req, res) => {
