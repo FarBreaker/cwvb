57fa2ecb3747546f8886cf7cbcaea3b38383d6c6
Alibi <lorenzo@pinkdonutstudio.com>
2022-04-09 20:11:51 +0200

    Added Jenkins groovy

---
+++ job-dsl/nodejs.groovy
@@ -0,0 +1,17 @@
+job('Test DSL'){
+    scm {
+        git('https://github.com/FarBreaker/cwvb.git'){ node->
+        node / gitConfigName('FarBreaker')
+        node / gitConfigEmail('lorenzo@pinkdonutstudio.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers{
+        nodejs('16.14.2')
+    }
+    steps{
+        shell('npm install')
+    }
+}
